0e0b88545420d004cee15e8867a48949f7642ea4
Cybozu Neco <cybozu-neco@users.noreply.github.com>
2026-02-10 07:55:18 +0000

    update

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1504a8c3.js',
+                path_to_root + 'searchindex-02e1cb57.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
